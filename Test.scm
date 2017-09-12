f3f9a91457f2a0e8547d811cad67a80d19f3f9a1
Thor Jensen <GingerV1k1ng@users.noreply.github.com>
2017-09-12 15:34:14 +0200

    Added some more functions
    Added more functions

--- Test.scm
+++ Test.scm
@@ -2,7 +2,7 @@
 (define (test? x) (+ 1 x))
 
 (define l (list 1 2 3))
-(define li (list a . b))
+(define lst (list a . b))
 
 (define (myList? x) (cdr((myList?(x)))))
 
@@ -19,8 +19,17 @@
       (and (pair? lst) (proper-list?(cdr lst)))))
 
 ;;;Gives you every second element of a list
-(define (every-second-element lst)
+(define (my-every-second-element lst)
   (cond ((null? lst) '())
         ((null? (cdr lst)) (list(car lst)))
-        (else (cons (car lst) (every-second-element(cdr (cdr lst)))))))
+        (else (cons (car lst) (my-every-second-element(cdr (cdr lst)))))))
+
+;;;Gives a specific number of tails, from at list, depending on what 'n' is
+(define (my-list-tail lst n)
+  (if(= n 0)lst
+     (my-list-tail(cdr lst) (- n 1))))
+;;Gives you a specific number of the first element of a list. 
+(define (my-list-prefix lst n)
+  (if( or (null? lst) (= n 0)) '()
+     (cons (car lst) (my-list-prefix(cdr lst) (- n 1)))))
 
